b8b28dfa7449b58bebc0bcf7922e43788ce0e583
Hamidreza Jafari <101067643+hamid-reza-jafari@users.noreply.github.com>
2022-05-10 14:24:10 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿let $ = document
+let myDiv = $.getElementById("contextMenu")
+
+$.body.addEventListener("click", () => {
+    myDiv.style.display = "none"
+})
+
+$.body.addEventListener("contextmenu", (e) => {
+    e.preventDefault()
+    myDiv.style.left = e.pageX + "px"
+    myDiv.style.top = e.pageY + "px"
+    myDiv.style.display = "inline-block"
+})
